d504c46d45ddd749dcd929d07d7b0e8588db6669
jaiver balanta <balantajaiver@gmail.com>
2019-08-02 22:18:22 -0500

    Inicio del proyecto

---
+++ src/triqui/FXML_GUIController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triqui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Administrador
+ */
+public class FXML_GUIController implements Initializable {
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
